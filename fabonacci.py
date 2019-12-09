11c207d3815cc7703a3a79a62f779e4fb563a94a
Bhanupratap.Singh <bhanupratap.singh@thomsonreuters.com>
2019-12-09 17:47:19 +0530

    optimised fabonacci.py

--- fabonacci.py
+++ fabonacci.py
@@ -1,25 +1,8 @@
-#this program is made to print fabonacci series
-
-while True:
-       print("Enter x for exit.")
-       terms = raw_input("upto how many terms? ")
-       if terms == 'x' :
-              break
-       else:
-              term = int(terms)
-              a = 0
-              b = 1
-              count = 2
-              if term <=0:
-                      print("print a positive number")
-              elif term == 1:
-                     print(a)
-              else:
-                     print(a)
-                     while count < term:
-                             c = a + b
-                             print(c)
-                             a = b 
-                             b = c
-                             count = count +1
-              print("\n")
+def fib(n):
+    a,b=1,2
+    while a < n:
+        print(a, end=" ")
+        a,b=b, a+b
+    print()
+x=input("enter number: ")
+fib(int(x))
